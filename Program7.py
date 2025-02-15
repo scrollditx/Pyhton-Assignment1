a2a9393a38bb3023e4e9e59af23f989c3639b65b
scrollditx <parekhveddharmeshbhai@gmail.com>
2025-02-15 16:48:29 +0530

    done

--- Program7.py
+++ Program7.py
@@ -1,2 +1,30 @@
 # WAP to Convert one datatype in to another datatype.
 
+integer_var = 11
+float_var = 4.11
+string_var = "123"
+boolean_var = True
+
+# Converting integer to float 
+int_to_float = float(integer_var)
+print(f"Converting integer {integer_var} to float : {int_to_float}")
+
+# Converting float to integer 
+float_to_int = int(float_var)
+print(f"Converting float {float_var} to integer : {float_to_int}")
+
+# Converting string to integer 
+string_to_int = int(string_var)
+print(f"Converting string {string_var} to integer : {string_to_int}")
+
+# Converting integer to string 
+int_to_string = int(string_var)
+print(f"Converting integer {string_var} to string : {int_to_string}")
+
+# Converting boolean to integer 
+boolean_to_int = int(boolean_var)
+print(f"Converting boolean {boolean_var} to integer : {boolean_to_int}")
+
+# Converting integer to boolean 
+int_to_boolean = bool(integer_var)
+print(f"Converting integer {integer_var} to boolean : {int_to_boolean}")
