52159a68ce6d6948eb19e41704a21ff963df19f2
scrollditx <parekhveddharmeshbhai@gmail.com>
2025-02-23 14:37:47 +0530

    Done

--- Program10.py
+++ Program10.py
@@ -1,1 +1,8 @@
 # WAP to pass a,b argument in class function and return the value of Class Function.
+
+def multiply(a,b):
+    ans = a*b
+    return ans
+
+result = multiply(5,10)
+print(f"Multiplication of 5 X 10 = {result}")
--- Program9.py
+++ Program9.py
@@ -1,1 +1,7 @@
 # WAP to perform default argument value of sum function.
+
+def sum(x,y=10):
+    ans = x + y
+    print(f"Sum of {x} and {y} is {ans}")
+
+sum(5)
